591026f613e415bd1b35250399892a18600a2ca8
Sameer Sheikh <sam.shekh0311@gmail.com>
2025-06-29 18:41:23 +0530

    regsiter page setup

---
+++ js/slider.js
@@ -0,0 +1,6 @@
+var offcanvasElementList = [].slice.call(
+  document.querySelectorAll(".offcanvas")
+);
+var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl);
+});
